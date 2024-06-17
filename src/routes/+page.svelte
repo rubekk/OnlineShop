2cb9d48ceac14cc299185005d6c8b09314b32d88
Rubek Maharjan <rubekmhzn7@gmail.com>
2024-06-17 08:57:40 +0545

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script></script>
+
+<div class="container">
+    <button class="add-product-btn">Add Product</button>
+    <form class="add-product-form">
+        <div class="inp-group">
+            <label for="">Product name</label>
+            <input type="text" placeholder="eg: Jai Ho Chamal">
+        </div>
+        <div class="inp-group">
+            <label for="">Price (in Rs.)</label>
+            <input type="number" placeholder="eg: 2450">
+        </div>
+        <div class="inp-group">
+            <label for="">Description</label>
+            <textarea></textarea>
+        </div>
+        <div class="inp-group">
+            <label for="">Upload image</label>
+            <input type="file">
+        </div>
+    </form>
+</div>
